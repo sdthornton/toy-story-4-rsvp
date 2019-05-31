bd15ae9aa2310ddc1b3f0cde255c8548df1f2fe6
Sam Thornton <sdthornton@live.com>
2019-05-31 10:56:48 -0700

    Fix ticket limit validation

--- app/models/reservations.rb
+++ app/models/reservations.rb
@@ -7,7 +7,7 @@ class Reservations < ActiveRecord::Base
   validate :tickets_cant_exceed_limit
 
   def tickets_cant_exceed_limit
-    if (Reservations.sum(:tickets) + self.tickets) >= TICKET_LIMIT
+    if (Reservations.sum(:tickets) + self.tickets) > TICKET_LIMIT
       errors.add(:tickets, "can't exceed limit")
     end
   end
